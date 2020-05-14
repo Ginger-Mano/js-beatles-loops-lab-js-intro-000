676d25fe88171576ac7f5e83f7af3ac2ecc19a0e
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-05-14 18:32:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function iLoveTheBeatles(num) {
   num = num + 1;
   }
   do {
-  array.push(`"I love the beatles!"`);
+  array.push("I love the beatles!");
   return array;
   }  while (num < 15); {
 }
